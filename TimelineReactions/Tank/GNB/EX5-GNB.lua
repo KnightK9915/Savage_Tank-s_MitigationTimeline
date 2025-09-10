cb749e6131f59ceeb46c5ffc5d8a88619efb603b
KnightK9915 <59334143+KnightK9915@users.noreply.github.com>
2025-09-10 17:00:01 +0900

    Add files via upload

--- TimelineReactions/Tank/GNB/EX5-GNB.lua
+++ TimelineReactions/Tank/GNB/EX5-GNB.lua
@@ -2692,7 +2692,7 @@ local tbl =
 			inheritedIndex = 1,
 		},
 	},
-	[47] = 
+	[44] = 
 	{
 		
 		{
@@ -2721,7 +2721,6 @@ local tbl =
 									true,
 								},
 							},
-							endIfUsed = true,
 							gVar = "ACR_RikuGNB3_CD",
 							ignoreWeaveRules = true,
 							name = "selfMarked",
@@ -2762,12 +2761,14 @@ local tbl =
 					},
 				},
 				eventType = 4,
-				mechanicTime = 210.7,
+				loop = true,
+				mechanicTime = 199.5,
 				name = "Record marked",
 				timeRange = true,
-				timelineIndex = 47,
-				timerStartOffset = -15,
-				uuid = "f11c5d30-9813-9392-bac1-a6c58fee1f85",
+				timelineIndex = 44,
+				timerEndOffset = 57.700000762939,
+				timerStartOffset = -5,
+				uuid = "7ab9e083-e657-a2a3-abdd-efe493685e42",
 				version = 2,
 			},
 			inheritedIndex = 1,
@@ -2796,7 +2797,6 @@ local tbl =
 									true,
 								},
 							},
-							endIfUsed = true,
 							gVar = "ACR_RikuGNB3_CD",
 							ignoreWeaveRules = true,
 							uuid = "94511374-01bd-2218-adca-8213e488a837",
@@ -2831,172 +2831,92 @@ local tbl =
 						},
 					},
 				},
-				mechanicTime = 210.7,
+				loop = true,
+				mechanicTime = 199.5,
 				name = "HoC (15)",
 				timeRange = true,
-				timelineIndex = 47,
+				timelineIndex = 44,
+				timerEndOffset = 57.700000762939,
 				timerOffset = -1,
-				timerStartOffset = -3,
-				uuid = "5b85a034-ced5-243d-85a4-0cf08c36d144",
+				timerStartOffset = -5,
+				uuid = "54e06d99-83e8-aa4f-83c3-5ad243b7125b",
 				version = 2,
 			},
-			inheritedIndex = 3,
-		},
-	},
-	[49] = 
-	{
-		
-		{
-			data = 
-			{
-				actions = 
-				{
-					
-					{
-						data = 
-						{
-							aType = "Lua",
-							actionID = 7533,
-							actionLua = "data.selfMarked = true\nself.used = true",
-							atomicPriority = true,
-							conditions = 
-							{
-								
-								{
-									"bf2d5893-2b7c-47a3-ad8b-cd55378c3028",
-									true,
-								},
-								
-								{
-									"d73a437b-daee-5b25-9e29-bfcac45843c2",
-									true,
-								},
-							},
-							endIfUsed = true,
-							gVar = "ACR_RikuGNB3_CD",
-							ignoreWeaveRules = true,
-							name = "selfMarked",
-							targetContentID = 13822,
-							targetType = "ContentID",
-							uuid = "a1aed48d-295b-da3e-978a-460aac09e87e",
-							version = 2.1,
-						},
-						inheritedIndex = 2,
-					},
-				},
-				conditions = 
-				{
-					
-					{
-						data = 
-						{
-							category = "Event",
-							eventArgType = 2,
-							eventMarkerID = 381,
-							uuid = "bf2d5893-2b7c-47a3-ad8b-cd55378c3028",
-							version = 2,
-						},
-					},
-					
-					{
-						data = 
-						{
-							category = "Party",
-							conditionType = 10,
-							inGroupTargetType = "Self",
-							name = "marker on Self",
-							partyTargetType = "Event Entity",
-							uuid = "d73a437b-daee-5b25-9e29-bfcac45843c2",
-							version = 2,
-						},
-						inheritedIndex = 3,
-					},
-				},
-				eventType = 4,
-				mechanicTime = 221.2,
-				name = "Record marked",
-				timeRange = true,
-				timelineIndex = 49,
-				timerEndOffset = 5,
-				timerStartOffset = -10.5,
-				uuid = "25744875-b99c-2de5-a46b-6cab89d21478",
-				version = 2,
-			},
-			inheritedIndex = 1,
-		},
-		
-		{
-			data = 
-			{
-				actions = 
-				{
-					
-					{
-						data = 
-						{
-							actionID = 25758,
-							conditions = 
-							{
-								
-								{
-									"aa47d4db-e3a7-7873-b33f-fc4b5b63ee52",
-									true,
-								},
-								
-								{
-									"2ca434c3-379c-8d67-a54a-24fbaf3c6775",
-									true,
-								},
-							},
-							endIfUsed = true,
-							gVar = "ACR_RikuGNB3_CD",
-							ignoreWeaveRules = true,
-							uuid = "94511374-01bd-2218-adca-8213e488a837",
-							version = 2.1,
-						},
-					},
-				},
-				conditions = 
-				{
-					
-					{
-						data = 
-						{
-							actionID = 25758,
-							category = "Self",
-							comparator = 2,
-							conditionType = 4,
-							dequeueIfLuaFalse = true,
-							uuid = "aa47d4db-e3a7-7873-b33f-fc4b5b63ee52",
-							version = 2,
-						},
-					},
-					
-					{
-						data = 
-						{
-							category = "Lua",
-							conditionLua = "return data.selfMarked",
-							name = "if self marked",
-							uuid = "2ca434c3-379c-8d67-a54a-24fbaf3c6775",
-							version = 2,
-						},
-					},
-				},
-				mechanicTime = 221.2,
-				name = "HoC (15)",
-				timeRange = true,
-				timelineIndex = 49,
-				timerOffset = -1,
-				timerStartOffset = -3,
-				uuid = "02dd48fe-82d0-056e-a0d0-478f85e56acb",
-				version = 2,
-			},
-			inheritedIndex = 3,
+			inheritedIndex = 2,
 		},
 	},
 	[53] = 
 	{
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							actionID = 25758,
+							conditions = 
+							{
+								
+								{
+									"aa47d4db-e3a7-7873-b33f-fc4b5b63ee52",
+									true,
+								},
+								
+								{
+									"3a5a728c-dfa9-638c-bfe4-223da52a45d3",
+									true,
+								},
+							},
+							endIfUsed = true,
+							gVar = "ACR_RikuGNB3_CD",
+							ignoreWeaveRules = true,
+							uuid = "94511374-01bd-2218-adca-8213e488a837",
+							version = 2.1,
+						},
+					},
+				},
+				conditions = 
+				{
+					
+					{
+						data = 
+						{
+							actionID = 25758,
+							category = "Self",
+							comparator = 2,
+							conditionType = 4,
+							dequeueIfLuaFalse = true,
+							uuid = "aa47d4db-e3a7-7873-b33f-fc4b5b63ee52",
+							version = 2,
+						},
+					},
+					
+					{
+						data = 
+						{
+							channelCheckSpellID = 44587,
+							conditionType = 7,
+							uuid = "3a5a728c-dfa9-638c-bfe4-223da52a45d3",
+							version = 2,
+						},
+						inheritedIndex = 2,
+					},
+				},
+				mechanicTime = 279,
+				name = "HoC (15)",
+				timeRange = true,
+				timelineIndex = 53,
+				timerOffset = -1,
+				timerStartOffset = -3,
+				uuid = "52bedcb7-057d-03d2-991c-de428a454e8f",
+				version = 2,
+			},
+			inheritedIndex = 3,
+		},
 		
 		{
 			data = 
@@ -3066,6 +2986,9 @@ local tbl =
 			},
 			inheritedIndex = 2,
 		},
+	},
+	[54] = 
+	{
 		
 		{
 			data = 
@@ -3125,13 +3048,154 @@ local tbl =
 						inheritedIndex = 2,
 					},
 				},
-				mechanicTime = 279,
+				mechanicTime = 284.1,
 				name = "HoC (15)",
 				timeRange = true,
-				timelineIndex = 53,
+				timelineIndex = 54,
 				timerOffset = -1,
 				timerStartOffset = -3,
-				uuid = "52bedcb7-057d-03d2-991c-de428a454e8f",
+				uuid = "b69bf747-08ae-cc96-86e5-2c25103495aa",
+				version = 2,
+			},
+			inheritedIndex = 3,
+		},
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							actionID = 36935,
+							conditions = 
+							{
+								
+								{
+									"30f02147-8e47-399f-99c5-279582aa248d",
+									true,
+								},
+								
+								{
+									"171ff7b9-7562-8548-8bcd-152922bcaf00",
+									true,
+								},
+							},
+							endIfUsed = true,
+							gVar = "ACR_RikuGNB3_CD",
+							ignoreWeaveRules = true,
+							uuid = "d00db0ec-8fe2-c41e-8ee5-1c53101aa3bb",
+							version = 2.1,
+						},
+					},
+				},
+				conditions = 
+				{
+					
+					{
+						data = 
+						{
+							actionID = 36935,
+							category = "Self",
+							comparator = 2,
+							conditionType = 4,
+							uuid = "30f02147-8e47-399f-99c5-279582aa248d",
+							version = 2,
+						},
+					},
+					
+					{
+						data = 
+						{
+							channelCheckSpellID = 44587,
+							conditionType = 7,
+							uuid = "171ff7b9-7562-8548-8bcd-152922bcaf00",
+							version = 2,
+						},
+						inheritedIndex = 2,
+					},
+				},
+				mechanicTime = 284.1,
+				name = "Great Nebula (40)",
+				timeRange = true,
+				timelineIndex = 54,
+				timerStartOffset = -5,
+				uuid = "f014daa9-d082-cd3e-87c1-2583e66991c6",
+				version = 2,
+			},
+			inheritedIndex = 4,
+		},
+	},
+	[55] = 
+	{
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							actionID = 25758,
+							conditions = 
+							{
+								
+								{
+									"aa47d4db-e3a7-7873-b33f-fc4b5b63ee52",
+									true,
+								},
+								
+								{
+									"3a5a728c-dfa9-638c-bfe4-223da52a45d3",
+									true,
+								},
+							},
+							endIfUsed = true,
+							gVar = "ACR_RikuGNB3_CD",
+							ignoreWeaveRules = true,
+							uuid = "94511374-01bd-2218-adca-8213e488a837",
+							version = 2.1,
+						},
+					},
+				},
+				conditions = 
+				{
+					
+					{
+						data = 
+						{
+							actionID = 25758,
+							category = "Self",
+							comparator = 2,
+							conditionType = 4,
+							dequeueIfLuaFalse = true,
+							uuid = "aa47d4db-e3a7-7873-b33f-fc4b5b63ee52",
+							version = 2,
+						},
+					},
+					
+					{
+						data = 
+						{
+							channelCheckSpellID = 44587,
+							conditionType = 7,
+							uuid = "3a5a728c-dfa9-638c-bfe4-223da52a45d3",
+							version = 2,
+						},
+						inheritedIndex = 2,
+					},
+				},
+				mechanicTime = 289.3,
+				name = "HoC (15)",
+				timeRange = true,
+				timelineIndex = 55,
+				timerOffset = -1,
+				timerStartOffset = -3,
+				uuid = "273e1b0f-dcf2-4162-87a7-07c819a1b8cd",
 				version = 2,
 			},
 			inheritedIndex = 3,
@@ -4689,6 +4753,8 @@ local tbl =
 	inheritedProfiles = 
 	{
 		"Tank\\EX5-TANKGENERAL",
+		"store\\anyone\\extremes\\necron\\modules\\core",
+		"store\\anyone\\extremes\\necron\\modules\\draws",
 	},
 	mapID = 1296,
 	version = 1,
