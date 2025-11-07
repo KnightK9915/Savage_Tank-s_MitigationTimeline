59289805d9aea56d25c559868f101181ce6db4d3
KnightK9915 <59334143+KnightK9915@users.noreply.github.com>
2025-11-07 13:06:13 +0900

    Add files via upload

---
+++ TimelineReactions/Tank/PLD/FRU-PLD-Muai.lua
@@ -0,0 +1,32 @@
+local tbl = 
+{
+	[58] = 
+	{
+		
+		{
+			data = 
+			{
+				name = "[Core] Lookaway",
+				uuid = "df3cd397-7197-629e-bc2c-acdfbd05309e",
+				version = 2,
+			},
+			inheritedObjectUUID = "726cf3b4-ac65-0eae-9e78-43c3d5a8a3e1",
+			inheritedOverwrites = 
+			{
+				timerEndOffset = -0.10000000149012,
+			},
+		},
+	},
+	inheritedProfiles = 
+	{
+		"Jackpot\\fru-Tank-Self-Pld",
+		"Tank\\FRU-TANKGENERAL-Muai",
+		"MuAi\\MuaiGuideFru",
+	},
+	mapID = 1238,
+	version = 5,
+}
+
+
+
+return tbl
