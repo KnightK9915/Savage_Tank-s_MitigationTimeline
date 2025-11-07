f8442d7154a80e2e7faf8a4fe6d9e26ef29af0de
KnightK9915 <59334143+KnightK9915@users.noreply.github.com>
2025-11-07 13:05:33 +0900

    Add files via upload

---
+++ TimelineReactions/Tank/DRK/FRU-DRK-Muai.lua
@@ -0,0 +1,15 @@
+local tbl = 
+{
+	inheritedProfiles = 
+	{
+		"Jackpot\\fru-Tank-Self-Drk",
+		"Tank\\FRU-TANKGENERAL-Muai",
+		"MuAi\\MuaiGuideFru",
+	},
+	mapID = 1238,
+	version = 5,
+}
+
+
+
+return tbl
