dd9d5f1b1deea66eb6347ed0baaf6d93df4374fa
KnightK9915 <59334143+KnightK9915@users.noreply.github.com>
2025-12-22 00:41:03 +0900

    Add files via upload

--- TimelineReactions/Tank/EX6-TANKGENERAL.lua
+++ TimelineReactions/Tank/EX6-TANKGENERAL.lua
@@ -3887,6 +3887,533 @@ local tbl =
 			},
 		},
 	},
+	[51] = 
+	{
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							actionID = 3540,
+							conditions = 
+							{
+								
+								{
+									"d509fc50-866e-82e0-a683-7f673386a79e",
+									true,
+								},
+								
+								{
+									"28cd0d5b-62ee-dcf0-9a27-a18b72f8e80d",
+									true,
+								},
+								
+								{
+									"a787167c-e665-8709-84d0-298a7dbe954a",
+									true,
+								},
+								
+								{
+									"fe9d14d5-610f-4042-801e-76d693903568",
+									true,
+								},
+							},
+							endIfUsed = true,
+							gVar = "ACR_RikuPLD3_CD",
+							ignoreWeaveRules = true,
+							uuid = "208d8d0c-038a-0156-ae05-da413fbdfa98",
+							version = 2.1,
+						},
+					},
+				},
+				conditions = 
+				{
+					
+					{
+						data = 
+						{
+							actionID = 3540,
+							category = "Self",
+							comparator = 2,
+							conditionType = 4,
+							uuid = "d509fc50-866e-82e0-a683-7f673386a79e",
+							version = 2,
+						},
+					},
+					
+					{
+						data = 
+						{
+							category = "Self",
+							conditionType = 14,
+							jobIDList = 
+							{
+								19,
+							},
+							uuid = "28cd0d5b-62ee-dcf0-9a27-a18b72f8e80d",
+							version = 2,
+						},
+					},
+					
+					{
+						data = 
+						{
+							buffCheckType = 5,
+							buffID = 1457,
+							buffIDList = 
+							{
+								1457,
+								1839,
+								1894,
+							},
+							category = "Self",
+							uuid = "a787167c-e665-8709-84d0-298a7dbe954a",
+							version = 2,
+						},
+					},
+					
+					{
+						data = 
+						{
+							category = "Lua",
+							conditionLua = "return (RikuduoGadget and RikuduoGadget.group_is(\"MTgroup\")) or false",
+							name = "GroupMit MT",
+							uuid = "fe9d14d5-610f-4042-801e-76d693903568",
+							version = 2,
+						},
+					},
+				},
+				mechanicTime = 515.1,
+				name = "Veil",
+				timeRange = true,
+				timelineIndex = 51,
+				timerOffset = -10,
+				timerStartOffset = -5.5,
+				uuid = "23eb480b-cd4e-fa8d-a715-a0d37ff509b2",
+				version = 2,
+			},
+			inheritedIndex = 1,
+		},
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							actionID = 16160,
+							conditions = 
+							{
+								
+								{
+									"a810d2af-ef39-5b67-9d5f-95b37d750c7a",
+									true,
+								},
+								
+								{
+									"115eca71-8e25-2c76-be17-c29c66dcf324",
+									true,
+								},
+								
+								{
+									"a6929435-5dcb-3407-8d89-b38a0596d1ac",
+									true,
+								},
+								
+								{
+									"36f592c9-8cdc-0f47-b369-1b8e813fc6f5",
+									true,
+								},
+							},
+							endIfUsed = true,
+							gVar = "ACR_RikuDRK3_CD",
+							ignoreWeaveRules = true,
+							uuid = "dbc99f8f-e1c5-f477-89e0-30fa7efeda14",
+							version = 2.1,
+						},
+					},
+				},
+				conditions = 
+				{
+					
+					{
+						data = 
+						{
+							actionID = 16160,
+							category = "Self",
+							comparator = 2,
+							conditionType = 4,
+							uuid = "a810d2af-ef39-5b67-9d5f-95b37d750c7a",
+							version = 2,
+						},
+					},
+					
+					{
+						data = 
+						{
+							category = "Self",
+							conditionType = 14,
+							jobIDList = 
+							{
+								37,
+							},
+							uuid = "115eca71-8e25-2c76-be17-c29c66dcf324",
+							version = 2,
+						},
+					},
+					
+					{
+						data = 
+						{
+							buffCheckType = 5,
+							buffID = 1457,
+							buffIDList = 
+							{
+								1457,
+								1894,
+								1362,
+							},
+							category = "Self",
+							uuid = "a6929435-5dcb-3407-8d89-b38a0596d1ac",
+							version = 2,
+						},
+					},
+					
+					{
+						data = 
+						{
+							category = "Lua",
+							conditionLua = "return (RikuduoGadget and RikuduoGadget.group_is(\"MTgroup\")) or false",
+							name = "GroupMit MT",
+							uuid = "36f592c9-8cdc-0f47-b369-1b8e813fc6f5",
+							version = 2,
+						},
+					},
+				},
+				mechanicTime = 515.1,
+				name = "HoL",
+				timeRange = true,
+				timelineIndex = 51,
+				timerStartOffset = -5.5,
+				uuid = "e882b800-8026-ce40-a624-61e25a51bcb5",
+				version = 2,
+			},
+			inheritedIndex = 4,
+		},
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							actionID = 16471,
+							conditions = 
+							{
+								
+								{
+									"142e7d5f-3fc7-8773-b581-c9fe1ad51671",
+									true,
+								},
+								
+								{
+									"322dd20f-009d-0bff-941f-f877ce997638",
+									true,
+								},
+								
+								{
+									"e17d5cf3-fee4-4a84-82d2-63f39144d1cb",
+									true,
+								},
+								
+								{
+									"77d0034f-3331-0d75-ab99-2ebfcaa0fdba",
+									true,
+								},
+							},
+							gVar = "ACR_RikuDRK3_CD",
+							ignoreWeaveRules = true,
+							uuid = "32970969-7f85-6bc8-90e1-4f30e8ce8093",
+							version = 2.1,
+						},
+					},
+				},
+				conditions = 
+				{
+					
+					{
+						data = 
+						{
+							actionID = 16471,
+							category = "Self",
+							comparator = 2,
+							conditionType = 4,
+							uuid = "142e7d5f-3fc7-8773-b581-c9fe1ad51671",
+							version = 2,
+						},
+					},
+					
+					{
+						data = 
+						{
+							category = "Self",
+							conditionType = 14,
+							jobIDList = 
+							{
+								32,
+							},
+							uuid = "322dd20f-009d-0bff-941f-f877ce997638",
+							version = 2,
+						},
+					},
+					
+					{
+						data = 
+						{
+							buffCheckType = 5,
+							buffID = 1457,
+							buffIDList = 
+							{
+								1457,
+								1362,
+								1839,
+							},
+							category = "Self",
+							uuid = "e17d5cf3-fee4-4a84-82d2-63f39144d1cb",
+							version = 2,
+						},
+						inheritedIndex = 3,
+					},
+					
+					{
+						data = 
+						{
+							category = "Lua",
+							conditionLua = "return (RikuduoGadget and RikuduoGadget.group_is(\"MTgroup\")) or false",
+							name = "GroupMit MT",
+							uuid = "77d0034f-3331-0d75-ab99-2ebfcaa0fdba",
+							version = 2,
+						},
+					},
+				},
+				mechanicTime = 515.1,
+				name = "Dark Missinary",
+				timeRange = true,
+				timelineIndex = 51,
+				timerOffset = -10,
+				timerStartOffset = -5.5,
+				uuid = "cabcb2b8-c6b4-3591-add2-7991295390e7",
+				version = 2,
+			},
+		},
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							actionID = 7388,
+							conditions = 
+							{
+								
+								{
+									"c59381a9-a0cc-60ea-8a36-f27fa7afb9c0",
+									true,
+								},
+								
+								{
+									"cfdfeb27-fd18-76d8-915e-3d53780d23cf",
+									true,
+								},
+								
+								{
+									"3e2075b4-140b-bff0-9500-2b8ae0ddf9da",
+									true,
+								},
+								
+								{
+									"4703d63f-1d9a-dc64-b876-1dd78bb9b7c8",
+									true,
+								},
+							},
+							gVar = "ACR_RikuDRK3_CD",
+							ignoreWeaveRules = true,
+							uuid = "83ae2ac2-4e1e-673e-befc-12d5802ce1cd",
+							version = 2.1,
+						},
+					},
+				},
+				conditions = 
+				{
+					
+					{
+						data = 
+						{
+							actionID = 7388,
+							category = "Self",
+							comparator = 2,
+							conditionType = 4,
+							uuid = "c59381a9-a0cc-60ea-8a36-f27fa7afb9c0",
+							version = 2,
+						},
+					},
+					
+					{
+						data = 
+						{
+							category = "Self",
+							conditionType = 14,
+							jobIDList = 
+							{
+								21,
+							},
+							uuid = "cfdfeb27-fd18-76d8-915e-3d53780d23cf",
+							version = 2,
+						},
+					},
+					
+					{
+						data = 
+						{
+							buffCheckType = 5,
+							buffID = 1457,
+							buffIDList = 
+							{
+								1362,
+								1839,
+								1894,
+							},
+							category = "Self",
+							uuid = "3e2075b4-140b-bff0-9500-2b8ae0ddf9da",
+							version = 2,
+						},
+						inheritedIndex = 3,
+					},
+					
+					{
+						data = 
+						{
+							category = "Lua",
+							conditionLua = "return (RikuduoGadget and RikuduoGadget.group_is(\"MTgroup\")) or false",
+							name = "GroupMit MT",
+							uuid = "4703d63f-1d9a-dc64-b876-1dd78bb9b7c8",
+							version = 2,
+						},
+					},
+				},
+				mechanicTime = 515.1,
+				name = "Shake it off",
+				timeRange = true,
+				timelineIndex = 51,
+				timerStartOffset = -5.5,
+				uuid = "d54a11a1-b5d7-5d7f-b222-25ff2e08c15d",
+				version = 2,
+			},
+			inheritedIndex = 4,
+		},
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							actionID = 7535,
+							conditions = 
+							{
+								
+								{
+									"5d8c1a06-2729-6214-9cb3-bf4683e9f41f",
+									true,
+								},
+								
+								{
+									"293daf40-34c7-3d12-ac20-7b597e340bdc",
+									true,
+								},
+								
+								{
+									"0a03e4a6-9793-cc80-88c7-9e3d941f565f",
+									true,
+								},
+							},
+							endIfUsed = true,
+							gVar = "ACR_RikuPLD3_CD",
+							ignoreWeaveRules = true,
+							targetType = "Current Target",
+							uuid = "c9c9cc18-aa2e-c48a-bebe-6ab6f4e7c114",
+							version = 2.1,
+						},
+					},
+				},
+				conditions = 
+				{
+					
+					{
+						data = 
+						{
+							buffCheckType = 2,
+							buffID = 1193,
+							uuid = "5d8c1a06-2729-6214-9cb3-bf4683e9f41f",
+							version = 2,
+						},
+					},
+					
+					{
+						data = 
+						{
+							comparator = 2,
+							conditionType = 6,
+							dequeueIfLuaFalse = true,
+							inRangeValue = 5,
+							uuid = "293daf40-34c7-3d12-ac20-7b597e340bdc",
+							version = 2,
+						},
+						inheritedIndex = 2,
+					},
+					
+					{
+						data = 
+						{
+							category = "Lua",
+							conditionLua = "return (RikuduoGadget and RikuduoGadget.group_is(\"MTgroup\")) or false",
+							name = "GroupMit MT",
+							uuid = "0a03e4a6-9793-cc80-88c7-9e3d941f565f",
+							version = 2,
+						},
+					},
+				},
+				enabled = false,
+				mechanicTime = 515.1,
+				name = "Reprisal",
+				randomOffset = -2,
+				timeRange = true,
+				timelineIndex = 51,
+				timerOffset = -3,
+				timerStartOffset = -5.5,
+				uuid = "957c1fe4-e06e-eb3a-bbd2-a90934531c28",
+				version = 2,
+			},
+		},
+	},
 	[52] = 
 	{
 		
@@ -4093,6 +4620,7 @@ local tbl =
 						data = 
 						{
 							actionID = 3540,
+							atomicPriority = true,
 							conditions = 
 							{
 								
@@ -4108,6 +4636,11 @@ local tbl =
 								
 								{
 									"a787167c-e665-8709-84d0-298a7dbe954a",
+									true,
+								},
+								
+								{
+									"61dc0255-6fdc-5492-94d4-ad0a99c468fe",
 									true,
 								},
 							},
@@ -4164,14 +4697,25 @@ local tbl =
 							version = 2,
 						},
 					},
+					
+					{
+						data = 
+						{
+							category = "Lua",
+							conditionLua = "return (RikuduoGadget and RikuduoGadget.group_is(\"STgroup\")) or false",
+							name = "GroupMit ST",
+							uuid = "61dc0255-6fdc-5492-94d4-ad0a99c468fe",
+							version = 2,
+						},
+					},
 				},
 				mechanicTime = 533.4,
 				name = "Veil",
 				timeRange = true,
 				timelineIndex = 53,
 				timerOffset = -10,
-				timerStartOffset = -6.5,
-				uuid = "9cccea27-b092-7e26-926d-b55cfa3ff928",
+				timerStartOffset = -4.5,
+				uuid = "cc65cb78-3749-4695-9e29-1624662d3013",
 				version = 2,
 			},
 			inheritedIndex = 1,
@@ -4202,6 +4746,11 @@ local tbl =
 								
 								{
 									"a6929435-5dcb-3407-8d89-b38a0596d1ac",
+									true,
+								},
+								
+								{
+									"3e97a91a-41dc-23ef-b713-ff17113b522b",
 									true,
 								},
 							},
@@ -4258,13 +4807,24 @@ local tbl =
 							version = 2,
 						},
 					},
+					
+					{
+						data = 
+						{
+							category = "Lua",
+							conditionLua = "return (RikuduoGadget and RikuduoGadget.group_is(\"STgroup\")) or false",
+							name = "GroupMit ST",
+							uuid = "3e97a91a-41dc-23ef-b713-ff17113b522b",
+							version = 2,
+						},
+					},
 				},
 				mechanicTime = 533.4,
 				name = "HoL",
 				timeRange = true,
 				timelineIndex = 53,
-				timerStartOffset = -6.5,
-				uuid = "3b0e448e-f382-f01f-a153-6bec231c9e8f",
+				timerStartOffset = -4.5,
+				uuid = "2782312d-95ae-d6a2-9355-27838b984922",
 				version = 2,
 			},
 			inheritedIndex = 4,
@@ -4295,6 +4855,11 @@ local tbl =
 								
 								{
 									"e17d5cf3-fee4-4a84-82d2-63f39144d1cb",
+									true,
+								},
+								
+								{
+									"dc9b5774-de30-3222-94bf-b9c78bef27f4",
 									true,
 								},
 							},
@@ -4351,14 +4916,25 @@ local tbl =
 						},
 						inheritedIndex = 3,
 					},
+					
+					{
+						data = 
+						{
+							category = "Lua",
+							conditionLua = "return (RikuduoGadget and RikuduoGadget.group_is(\"STgroup\")) or false",
+							name = "GroupMit ST",
+							uuid = "dc9b5774-de30-3222-94bf-b9c78bef27f4",
+							version = 2,
+						},
+					},
 				},
 				mechanicTime = 533.4,
 				name = "Dark Missinary",
 				timeRange = true,
 				timelineIndex = 53,
 				timerOffset = -10,
-				timerStartOffset = -6.5,
-				uuid = "eed0ff78-be32-dc44-a526-5757f56fe928",
+				timerStartOffset = -4.5,
+				uuid = "edaab8eb-5a49-9b52-9412-4a0e9c79463f",
 				version = 2,
 			},
 		},
@@ -4388,6 +4964,11 @@ local tbl =
 								
 								{
 									"3e2075b4-140b-bff0-9500-2b8ae0ddf9da",
+									true,
+								},
+								
+								{
+									"ba76c676-ab5b-698a-b170-aa16cfdef928",
 									true,
 								},
 							},
@@ -4444,13 +5025,24 @@ local tbl =
 						},
 						inheritedIndex = 3,
 					},
+					
+					{
+						data = 
+						{
+							category = "Lua",
+							conditionLua = "return (RikuduoGadget and RikuduoGadget.group_is(\"STgroup\")) or false",
+							name = "GroupMit ST",
+							uuid = "ba76c676-ab5b-698a-b170-aa16cfdef928",
+							version = 2,
+						},
+					},
 				},
 				mechanicTime = 533.4,
 				name = "Shake it off",
 				timeRange = true,
 				timelineIndex = 53,
-				timerStartOffset = -6.5,
-				uuid = "ff6914f4-07c5-4872-b36d-e522f9135503",
+				timerStartOffset = -4.5,
+				uuid = "25b0999c-fd7c-7405-b501-bddb4d47e04f",
 				version = 2,
 			},
 			inheritedIndex = 4,
@@ -4514,7 +5106,6 @@ local tbl =
 						inheritedIndex = 2,
 					},
 				},
-				enabled = false,
 				mechanicTime = 533.4,
 				name = "Reprisal",
 				randomOffset = -2,
@@ -4522,7 +5113,7 @@ local tbl =
 				timelineIndex = 53,
 				timerOffset = -3,
 				timerStartOffset = -4.5,
-				uuid = "dfca6cfa-d437-a4d9-806d-a02ec4dd7d87",
+				uuid = "43ea7a8f-9be1-42d6-9b11-449edbc895f5",
 				version = 2,
 			},
 		},
